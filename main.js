556949357ab8b053ca4e09f3e31e86980b859615
arifin616 <153440101+arifin616@users.noreply.github.com>
2024-01-17 17:53:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        int result = 0;
+        
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+        
+        System.out.println("Result: " + result);
+    }
+}
